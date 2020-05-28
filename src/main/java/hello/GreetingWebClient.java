c74887adbd42b7ac612cb8f9860ae3338e51010a
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-05-28 19:40:32 -0400

    added app

--- src/main/java/hello/GreetingWebClient.java
+++ src/main/java/hello/GreetingWebClient.java
@@ -3,7 +3,6 @@ package hello;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import reactor.core.publisher.Mono;
 
 public class GreetingWebClient {
